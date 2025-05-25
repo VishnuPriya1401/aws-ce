47dd3b59f3ad47e24825fc20d8a5dec076801a12
VishnuPriya1401 <eakpriya14@gmail.com>
2025-05-25 14:36:54 +0530

    App-file-setup

---
+++ src/Login.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div className='input-area'>
+        <div>
+            <form action="">
+                <div className="input-sec">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" placeholder='Enter your Email' />
+                </div>
+                <div className="input-sec">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder='Enter Password' />
+                </div>
+                <button className='btn btn-success'>Login</button>
+                <p>New User ?</p><p className='btn-reg'>Register</p>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default login
